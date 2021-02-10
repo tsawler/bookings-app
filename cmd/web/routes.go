1d695622b313d579cad7375303229c6f9b1e69dc
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-10 07:04:44 -0400

    static assets

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,5 +18,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }

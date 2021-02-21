2775f9e1856c1c7c4122eebd436c1e0a5c514a48
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-21 19:24:32 -0400

    Tests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,6 +20,25 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
@@ -38,6 +57,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -47,16 +67,5 @@ func main() {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
-
-	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	return nil
 }
---
+++ cmd/web/routes_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/tsawler/bookings-app/internal/config"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+	var app config.AppConfig
+
+	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case *chi.Mux:
+		// do nothing
+	default:
+		t.Error(fmt.Sprintf("type is not *chi.Mux, but is %T", v))
+	}
+}

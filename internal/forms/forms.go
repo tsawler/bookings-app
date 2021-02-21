43ee248a7f0855947cc3ae767f3d1a8e476bd846
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-21 17:54:33 -0400

    Forms and validation (server side)

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -40,7 +40,6 @@ func (f *Form) Required(fields ...string) {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	}
 	return true

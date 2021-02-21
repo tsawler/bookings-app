e5fd262ba7443d81a8df6a0188529545ef0ee28f
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-21 16:42:19 -0400

    Making an ajax request

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,7 +22,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
-	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
 	mux.Get("/contact", handlers.Repo.Contact)
 

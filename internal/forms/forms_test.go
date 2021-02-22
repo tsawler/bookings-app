f93b80b0e668c406c72c38a624537519b539a0e5
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-22 07:26:22 -0400

    Testing forms package

---
+++ internal/forms/forms_test.go
@@ -0,0 +1,42 @@
+package forms
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Valid(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	if form.Valid() {
+		t.Error("form shows valid when required fields missing")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	postedData.Add("b", "a")
+	postedData.Add("c", "a")
+
+	r, _ = http.NewRequest("POST", "/whatever", nil)
+
+	r.PostForm = postedData
+	form = New(r.PostForm)
+	form.Required("a", "b", "c")
+	if !form.Valid() {
+		t.Error("shows does not have required fields when it does")
+	}
+}
